4ca095df39d356da6d3490cd84befd4bd5199ff1
sunyx <yong_xing2008@126.com>
2018-12-26 18:09:18 +0800

    spring-hello

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -2,6 +2,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Calendar;
+
 /**
  * Created by liuli on 2018/12/26.
  */
@@ -13,4 +15,14 @@ public class TestCase {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(conf);
         System.out.println("123:"+applicationContext);
     }
+    @Test
+    public void test2()
+    {
+        String conf = "applicationContext.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(conf);
+        Calendar calendar = applicationContext.getBean("calendarObj1", Calendar.class);
+        System.out.println("123:"+calendar);
+    }
+
+
 }
